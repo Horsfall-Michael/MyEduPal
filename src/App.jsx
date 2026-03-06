121d051a7cd46a5c4fc1460b52c8c1fda5585bbf
Horsfall Michael <horsfallmichael423@gmail.com>
2026-03-06 20:33:58 +0100

    Add Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { LogIn } from './Pages/LogIn';
+
+import {React, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
 import './App.css'
 
 function App() {
   return (
     <>
+    <Routes>
+      <Route index element={<LogIn />} />
+    </Routes>
     </>
   )
 }
